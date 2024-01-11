8b686c095e33b5b07fece72a37346d90c435c736
maxmax95 <onagaonaga12@gmail.com>
2024-01-11 11:19:05 -0300

    migrations feitas, com relacionamentos

---
+++ database/migrations/2024_01_11_132756_create_table_user_profile.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableUserProfile extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_profile', function (Blueprint $table) {
+            $table->increments('id');
+            $table->text('about')->nullable(true);
+            $table->string('social_medias')->nullable(true);
+            $table->string('phone');
+            $table->string('mobile_phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_profile');
+    }
+}
